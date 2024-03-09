a381231757e88a19f8db1fc2cc584f43dde91399
jeongdonghee <dh.jeong@ex-em.com>
2024-03-09 14:23:56 +0900

    Init project

---
+++ src/main/java/project/dhjeong/autoexpense/AutoExpenseApplication.java
@@ -0,0 +1,13 @@
+package project.dhjeong.autoexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoExpenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/dhjeong/autoexpense/AutoExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package project.dhjeong.autoexpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
